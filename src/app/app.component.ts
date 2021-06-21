85f1b88fe8111584ab786a00b3ba1480ae222da2
minhazv14 <47391006+minhazv14@users.noreply.github.com>
2021-06-21 21:42:16 +0600

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {UsersService}  from './users.service'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'blog';
+  data:any
+
+
+  constructor (private user:UsersService){
+    this.user.getData().subscribe(data=>{
+       console.warn(data)
+       this.data=data
+      
+    })
+  }
+
+}
